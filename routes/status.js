b154b03b9d8ec21fdcd3ecbed2873955cb22fa48
Eimi Okuno <Eimi.Okuno@bbc.co.uk>
2019-07-05 09:12:53 +0100

    React component convention to general js

--- routes/status.js
+++ routes/status.js
@@ -1,11 +1,11 @@
-const MessageBroker = require('../components/MessageBroker');
+const messageBroker = require('../lib/messageBroker');
 
 module.exports = {
   healthCheck: (req, res) => {
     res.sendStatus(200);
   },
   sendMessage: (req, res) => {
-    MessageBroker.publish('Hello from Digital Paper Edit API', (err, data) => {
+    messageBroker.publish('Hello from Digital Paper Edit API', (err, data) => {
       if (err) {
         res.sendStatus(500).json({ status: 'not ok', message: err });
       } else {
