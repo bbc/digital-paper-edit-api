b8cc3df136838f0f795fa350d8cbeca278eb644e
Allison Shultes <allison.shultes@bbc.co.uk>
2019-07-15 14:56:36 +0100

    Refactor: Cleans up code in seed files with linting

--- knex/seeds/03_projects.js
+++ knex/seeds/03_projects.js
@@ -1,27 +1,24 @@
-exports.seed = function (knex, Promise) {
-  return knex('Projects').del()
-    .then(() =>
-      knex('Projects').insert([
-        {
-          id: 1,
-          title: 'Project One',
-          description: 'Description Project One',
-          created_at: '2019-06-22 19:10:25-07',
-          updated_at: '2019-06-23 09:10:25-07',
-        },
-        {
-          id: 2,
-          title: 'Project Two',
-          description: 'Description Project Two',
-          created_at: '2019-06-21 19:10:25-07',
-          updated_at: '2019-06-22 09:10:25-07',
-        },
-        {
-          id: 3,
-          title: 'Project Three',
-          description: 'Description Project Three',
-          created_at: '2019-06-20 09:10:25-07',
-          updated_at: '2019-06-21 19:10:25-07',
-        },
-      ]));
-};
+exports.seed = knex => knex('Projects').del()
+  .then(() => knex('Projects').insert([
+    {
+      id: 1,
+      title: 'Project One',
+      description: 'Description Project One',
+      created_at: '2019-06-22 19:10:25-07',
+      updated_at: '2019-06-23 09:10:25-07',
+    },
+    {
+      id: 2,
+      title: 'Project Two',
+      description: 'Description Project Two',
+      created_at: '2019-06-21 19:10:25-07',
+      updated_at: '2019-06-22 09:10:25-07',
+    },
+    {
+      id: 3,
+      title: 'Project Three',
+      description: 'Description Project Three',
+      created_at: '2019-06-20 09:10:25-07',
+      updated_at: '2019-06-21 19:10:25-07',
+    },
+  ]));
--- knex/seeds/04_labels.js
+++ knex/seeds/04_labels.js
@@ -1,29 +1,27 @@
-exports.seed = function (knex, Promise) {
-  return knex('Labels').del()
-    .then(() => knex('Labels').insert([
-      {
-        id: 1,
-        title: "default",
-        colour: "orange",
-        description: "A default label",
-      },
-      {
-        id: 2,
-        title: "economics",
-        colour: "brown",
-        description: "Test description 1",
-      },
-      {
-        id: 3,
-        title: "Introduction",
-        colour: "darkblue",
-        description: "Test description 2",
-      },
-      {
-        id: 4,
-        title: "Brexit",
-        colour: "#FF5630",
-        description: "Brexit description",
-      },
-      ]));
-};
+exports.seed = knex => knex('Labels').del()
+  .then(() => knex('Labels').insert([
+    {
+      id: 1,
+      title: 'default',
+      colour: 'orange',
+      description: 'A default label',
+    },
+    {
+      id: 2,
+      title: 'economics',
+      colour: 'brown',
+      description: 'Test description 1',
+    },
+    {
+      id: 3,
+      title: 'Introduction',
+      colour: 'darkblue',
+      description: 'Test description 2',
+    },
+    {
+      id: 4,
+      title: 'Brexit',
+      colour: '#FF5630',
+      description: 'Brexit description',
+    },
+  ]));
