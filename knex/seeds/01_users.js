310502ec44b5e535f4845d58bbce0510e534378f
Allison Shultes <allison.shultes@bbc.co.uk>
2019-07-15 12:56:21 +0100

    WIP (but working): User table seed file

---
+++ knex/seeds/01_users.js
@@ -0,0 +1,18 @@
+exports.seed = function (knex, Promise) {
+  return knex('Users').del()
+    .then(() =>
+      knex('Users').insert([
+        {
+          email: 'nigel@email.com',
+          name: 'Nigel One',
+        },
+        {
+          email: 'nakaz@email.com',
+          name: 'Nakaz Two',
+        },
+        {
+          email: 'jaywon@email.com',
+          name: 'Jaywon Three',
+        },
+      ]));
+};
